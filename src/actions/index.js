8c44f102bbbf07edd6916a46c9ff84c3d9384606
mcabezas <ext_mcabezas@mercadolibre.com>
2018-11-14 14:28:40 -0300

    First commit of weather app

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_KEY = '750e5e9f62eb47529a1e8e489e4fe0c6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+
+}
